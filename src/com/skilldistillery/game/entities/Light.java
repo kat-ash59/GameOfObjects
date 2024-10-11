ff5a7adda58e32436d879773b76e16639bfa3e96
kat-ash59 <kkasmieh@hotmail.com>
2024-10-11 15:09:56 -0600

    adding in changes for Light.java

--- src/com/skilldistillery/game/entities/Light.java
+++ src/com/skilldistillery/game/entities/Light.java
@@ -1,17 +1,33 @@
 package com.skilldistillery.game.entities;
 
 // This class is not part of the MVP
-public class Light extends Equipment {
+public class Light extends Equipment 
+{
 
 	private Brightness brightnessLevel;
 
-	public Light(String name) {
+	public Light(String name) 
+	{
 		super(name);
+		brightnessLevel = Brightness.MEDIUM;
 
 	}
+	
+	public Light(String name, Brightness brightnessLevel)
+	{
+		super(name);
+		this.brightnessLevel = brightnessLevel;
+	}
 
-	public void changeBrightnessLevel(Brightness brightnessLevel) {
-
+	public void changeBrightnessLevel(Brightness brightnessLevel) 
+	{
+		this.brightnessLevel = brightnessLevel;
+	}
+	
+	public Light getLight()
+	{
+		Light myLight = new Light(this.name,this.brightnessLevel);
+		return myLight;
 	}
 
 }
