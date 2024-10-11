3ab4335a2870a28d3074a2296c322b95fea24a83
kat-ash59 <kkasmieh@hotmail.com>
2024-10-10 19:53:41 -0600

    completed Snipe.java

--- src/com/skilldistillery/game/entities/Snipe.java
+++ src/com/skilldistillery/game/entities/Snipe.java
@@ -1,6 +1,10 @@
 package com.skilldistillery.game.entities;
 
-public class Snipe extends Character{
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Snipe extends Character
+{
 	
 	private SnipeType snipe;
 
@@ -12,20 +16,60 @@ public class Snipe extends Character{
 		
 	public void hide() 
 	{
-		
+		System.out.println("OHHHH!!!! The Snipe vanished into thin air!");
 	}
 	
 	public int damage(Player player) 
 	{
-		return 0;
+		int randomIndex = 0;
+		Random randomNumber = new Random();
+		int healthPoints = player.getHealthPoints();
+		
+		randomIndex = randomNumber.nextInt(3);
+		if (randomIndex == 1)
+		{
+			healthPoints-=10;
+		}
+		else if (randomIndex == 2)
+		{
+			healthPoints-=20;
+		}
+		else
+		{
+			healthPoints-=30;
+		}
+		
+		
+		return healthPoints;
 	}
 	
-	public void  getCaught() {
-		
+	public void  getCaught() 
+	{
+		grantWish();
+		System.out.println("Thank-you for playing our Snip Hunt Game!!!");
+		System.out.println("Hope you had a great time!");
+		System.exit(0);
 	}
 	
-	public void  grantWish() {
-		
+	public void  grantWish() 
+	{
+		System.out.println("Darn it you caught me!!!");
+		System.out.println("I will now grant you one wish");
+		System.out.println("Say your wish to yourself ....");
+		try 
+		{
+			Thread.sleep(20);
+		} 
+		catch (InterruptedException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("\n\nYour Wish Is Granted!!!!\n\n");
 	}
 
+	public SnipeType getSnipeType() 
+	{
+		return snipe;
+	}
 }
