9c717bf9e272920d9961a1d5ff5f6bc23d6d9160
Abner Bouomo <abnerbouomo@gmail.com>
2024-10-12 11:34:03 -0400

    changes in Snipe

--- src/com/skilldistillery/game/entities/Snipe.java
+++ src/com/skilldistillery/game/entities/Snipe.java
@@ -16,30 +16,36 @@ public class Snipe extends Character {
 	}
 
 	public int damage() {
-		//FIXME damage should return a damage value and not handle modify the state of Player 
-		Random random = new Random(); 
-	    int damage = (random.nextInt(3) + 1) * 10;  // random number from 0-2 and damage: 10, 20, or 30
-	    return damage;
+
+		Random random = new Random();
+		int damage = (random.nextInt(3) + 1) * 10; // random number from 0-2 and damage: 10, 20, or 30
+		return damage;
 	}
 
 	public void getCaught() {
+		System.out.println("Darn it! You caught me!!!");
 		grantWish();
-		System.out.println("Thank-you for playing our Snip Hunt Game!!!");
+		System.out.println("Thank-you for playing our Snipe Hunt Game!");
 		System.out.println("Hope you had a great time!");
 		System.exit(0);
 	}
 
 	public void grantWish() {
-		System.out.println("Darn it you caught me!!!\n");
-		System.out.println("I will now grant you one wish\n");
-		System.out.println("Say your wish to yourself .... and count to 3\n");
+		System.out.println("As promised, I will grant you one wish...\n");
+		System.out.println("Close your eyes and say your wish aloud. I’ll count to 3...\n");
 		try {
-			Thread.sleep(300);
+			Thread.sleep(2000); // Wait is 2 seconds
+			System.out.println("1...");
+			Thread.sleep(1000); // Wait 1 second between counts
+			System.out.println("2...");
+			Thread.sleep(1000);
+			System.out.println("3...");
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("\n\nYour Wish Is Granted!!!!\n\n");
+
+		System.out.println("\n\nYour Wish Is Granted!!!\n\n");
 	}
 
 	public SnipeType getSnipeType() {
